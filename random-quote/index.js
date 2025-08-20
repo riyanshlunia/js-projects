061ca86e58673107b35c55692a13b19618d7bf8c
Riyansh <riyansh.lunia20@gmail.com>
2025-08-20 11:33:34 +0530

    random quote

--- random-quote/index.js
+++ random-quote/index.js
@@ -15,7 +15,15 @@ const usedIndexes = new Set()
 const quoteElement = document.getElementById("quote")
 
 function generateQuote(){
-    const randomIdx = Math.floor(Math.random()*quotes.length);
-    const quote = quotes[randomIdx];
-    quoteElement.innerHTML = quote;
+    if (usedIndexes.size >= quotes.length) {
+        usedIndexes.clear()
+    }
+    while(true){
+        const randomIdx = Math.floor(Math.random()*quotes.length);
+        if(usedIndexes.has(randomIdx)) continue
+        const quote = quotes[randomIdx];
+        quoteElement.innerHTML = quote;
+        usedIndexes.add(randomIdx);
+        break;
+    }
 }
